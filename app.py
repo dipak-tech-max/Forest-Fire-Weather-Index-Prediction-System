23b68c737edcc10414c72201d4560f955e12ad38
Dipak kumar <kumarfial@gmail.com>
2026-09-04 16:10:54 +0530

    Update model loading to use os.path.join
    Refactor model loading to use os.path.join for file paths.

--- app.py
+++ app.py
@@ -8,9 +8,10 @@ app = Flask(__name__)
 
 
 # Load the trained model
-ridge_model=pickle.load(open('Models\\forest_fire_Ml_Model_ridge_pickle_file','rb'))
+import os
 
-standard_scaler=pickle.load(open('Models\\forest_fire_scaler_pickle_file','rb'))
+ridge_model = pickle.load(open(os.path.join('Models', 'forest_fire_Ml_Model_ridge_pickle_file'), 'rb'))
+standard_scaler = pickle.load(open(os.path.join('Models', 'forest_fire_scaler_pickle_file'), 'rb'))
 
 # route for home page
 @app.route('/')
